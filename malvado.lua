d71f07ea7e3ebbff427b228d84d584afec29ed00
jepemo <Jeremias.Perez@desfici.com>
2017-06-26 22:55:15 +0200

    refactoring

--- malvado.lua
+++ malvado.lua
@@ -1,16 +1,35 @@
---
---  ______        _                _
--- |  ___ \      | |              | |
--- | | _ | | ____| |_   _ ____  _ | | ___
--- | || || |/ _  | | | | / _  |/ || |/ _ \
--- | || || ( ( | | |\ V ( ( | ( (_| | |_| |
--- |_||_||_|\_||_|_| \_/ \_||_|\____|\___/
---
---
+--[[
+  ______        _                _
+ |  ___ \      | |              | |
+ | | _ | | ____| |_   _ ____  _ | | ___
+ | || || |/ _  | | | | / _  |/ || |/ _ \
+ | || || ( ( | | |\ V ( ( | ( (_| | |_| |
+ |_||_||_|\_||_|_| \_/ \_||_|\____|\___/
+
+
+ malvado - A game programming library with  "DIV Game Studio"-style
+            processes for Lua/Love2D implemented in C with differents
+            bindings for other languages.
+
+ Copyright (C) 2017-present Jeremies Pérez Morata
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
 
 VERSION = 0.1
 
-proc_counter = 0
+local proc_counter = 0
 
 local MalvadoProcess = {
   ident = 0,
@@ -21,14 +40,14 @@ local MalvadoProcess = {
   z = 0,
 }
 
-MalvadoProcess.__index = MalvadoProcess
 MalvadoProcess.__call = function ()
   malvado.proc_count = malvado.proc_count + 1
-  self.ident = malvado.proc_count
-  table.insert(malvado.processes, proc)
+  --self.ident = malvado.proc_count
+  --table.insert(malvado.processes, proc)
 end
 
 function MalvadoProcess:new(ident, func)
+  self.__index = self
   return setmetatable({
     ident = ident,
     func = func,
